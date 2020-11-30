8a523827e6d5e9df378a98c360d1467936ebda9f
kidelectric <brian.isett@gmail.com>
2020-11-29 20:01:25 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project files for RFCX sound detection kaggle competition.',
+    author='Brian',
+    license='MIT',
+)
